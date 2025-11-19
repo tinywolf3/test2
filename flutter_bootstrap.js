b354b9942401fd234aca955b9a2e817433fbe215
tinywolf3 <tinywolf@gmail.com>
2025-11-19 13:26:29 +0900

    테스트 웹

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4005711311"
+    serviceWorkerVersion: "2155427222"
   }
 });
